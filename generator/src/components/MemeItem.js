9a753a23ca94ca811571d57b24193ad0c600d33a
crossmw\kmai <kmai@crossmedia.tv>
2018-01-04 15:49:38 -0500

    animations

--- generator/src/components/MemeItem.js
+++ generator/src/components/MemeItem.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 
 class MemeItem extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      hovered: false
+    }
+  }
+
   render(){
     return(
-      <div className="meme-item">
+      <div 
+        className="meme-item"
+        onMouseEnter={() => this.setState({hovered: true})}
+        onMouseLeave={() => this.setState({hovered: false})}
+      >
         <img
           src={this.props.meme.url}
           alt={this.props.meme.name}
-          className="meme-img"
+          className={this.state.hovered ? "meme-img darken-img" : "meme-img"}
         />
-        <p className="meme-txt">
+        <p className={this.state.hovered ? "meme-txt" : "no-txt"}>
           {this.props.meme.name}
         </p>
       </div>
